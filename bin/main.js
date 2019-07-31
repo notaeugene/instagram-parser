658b60ce8c613cf08b6919e24a553ff52f65a66d
Yevhenii Moskalenko <zh.mskl9@gmail.com>
2019-07-31 11:38:10 +0300

    Add base logic to save images

--- bin/main.js
+++ bin/main.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+
 const program = require('commander');
 const puppeteer = require('puppeteer');
+const shortid = require('shortid');
 
 program
   .option('-u, --username <username>', 'Set username')
+  // .option('-o, --offset [offset]', 'Set images offset')
+  // .option('-l, --limit [limit]', 'Set limit of images')
   .parse(process.argv);
 
 (async () => {
@@ -14,7 +19,30 @@ program
 
   await page.goto(`https://instagram.com/${username}`);
 
-  console.log(username);
+  // Get list images
+  const imgSourceList = await page.evaluate(() => {
+    const imgSelector = 'main[role="main"] article img';
+    const imgElementsList = Array.from(document.querySelectorAll(imgSelector));
+    return imgElementsList.map(img => img.src);
+  });
+
+  // Get image by url
+  const getImgByUrl = async url => {
+    const viewSource = await page.goto(url);
+    const buffer = await viewSource.buffer();
+    const filename = shortid.generate();
+
+    return { filename, buffer };
+  };
+
+  // Save images to disk
+  for (let url of imgSourceList) {
+    const { filename, buffer } = await getImgByUrl(url);
+
+    await fs.writeFile(`${filename}.jpg`, buffer, (err) => {
+      if (err) throw err;
+    });
+  }
 
   await browser.close();
 })();
