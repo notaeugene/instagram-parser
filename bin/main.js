64a002601157dcca5eb2085cf59fa3810dda80ae
Yevhenii Moskalenko <zh.mskl9@gmail.com>
2019-08-01 09:26:58 +0300

    Add destination folder option

--- bin/main.js
+++ bin/main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 
 const program = require('commander');
 const puppeteer = require('puppeteer');
@@ -8,18 +9,21 @@ const shortid = require('shortid');
 
 program
   .option('-u, --username <username>', 'Set username')
+  .option('-d, --dest <dest>', 'Set destionation folder')
   // .option('-o, --offset [offset]', 'Set images offset')
   // .option('-l, --limit [limit]', 'Set limit of images')
   .parse(process.argv);
 
+const IMG_DEST = program.dest || process.cwd();
+const PROFILE_URL = `https://instagram.com/${program.username}`;
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const { username } = program;
 
-  await page.goto(`https://instagram.com/${username}`);
+  await page.goto(PROFILE_URL);
 
-  // Get list images
+  // Get list of images
   const imgSourceList = await page.evaluate(() => {
     const imgSelector = 'main[role="main"] article img';
     const imgElementsList = Array.from(document.querySelectorAll(imgSelector));
@@ -36,10 +40,17 @@ program
   };
 
   // Save images to disk
+  if (program.dest) {
+    await fs.mkdir(IMG_DEST, (err) => {
+      if (err) throw err;
+    });
+  }
+
   for (let url of imgSourceList) {
     const { filename, buffer } = await getImgByUrl(url);
+    const imgPath = path.resolve(IMG_DEST, `${filename}.jpg`);
 
-    await fs.writeFile(`${filename}.jpg`, buffer, (err) => {
+    await fs.writeFile(imgPath, buffer, (err) => {
       if (err) throw err;
     });
   }
